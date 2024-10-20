b6646d7528e29153c23c3ec66378bae6394d45aa
Robocoders <team@blackbox.ai>
2024-10-20 10:49:33 +0000

    Added user authentication: models, serializers, views, and URLs

--- tubonge_backend/tubonge_backend/settings.py
+++ tubonge_backend/tubonge_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'messages',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- tubonge_backend/users/views.py
+++ tubonge_backend/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

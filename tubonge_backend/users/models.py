b07fa56b9316a770622fdeb1875ebe6880c1945a
Robocoders <team@blackbox.ai>
2024-10-20 10:52:53 +0000

    Resolved reverse accessor clashes in CustomUser model

--- tubonge_backend/users/models.py
+++ tubonge_backend/users/models.py
@@ -3,4 +3,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
